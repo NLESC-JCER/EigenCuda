9a5525aaa7ddd64e886a028adb77de74f482c7ed
felipe <tifonzafel@gmail.com>
2019-05-10 16:39:48 +0200

    clang-formatted

--- src/tests/test_dot.cc
+++ src/tests/test_dot.cc
@@ -1,20 +1,17 @@
 #define BOOST_TEST_MODULE eigen_cuda
-#include <boost/test/included/unit_test.hpp>
 #include "eigencuda.hpp"
-
+#include <boost/test/included/unit_test.hpp>
 
 using eigencuda::Mat;
 
 BOOST_AUTO_TEST_CASE(dot_product) {
-eigencuda::EigenCuda<float> EC;
-Mat<float> A = Mat<float>::Zero(2, 2);
-Mat<float> B = Mat<float>::Zero(2, 2);
+  eigencuda::EigenCuda<float> EC;
+  Mat<float> A = Mat<float>::Zero(2, 2);
+  Mat<float> B = Mat<float>::Zero(2, 2);
 
- A << 1., 2.,
-      3., 4.; 
- B << 5., 6.,
-      7., 8.;
+  A << 1., 2., 3., 4.;
+  B << 5., 6., 7., 8.;
 
- Mat<float> C = EC.dot(A, B);
- BOOST_CHECK_EQUAL(C.sum(), 134.);
+  Mat<float> C = EC.dot(A, B);
+  BOOST_CHECK_EQUAL(C.sum(), 134.);
 }
