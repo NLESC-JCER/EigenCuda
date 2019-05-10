f543b516eecdc078c024561e17ea94c4908f53ed
felipez <tifonzafel@gmail.com>
2019-05-10 14:10:02 +0200

    split the resource management from the naive matmul implementation

---
+++ include/matmul.hpp
@@ -0,0 +1,170 @@
+#ifndef EIGENMATMUL_H_
+#define EIGENMATMUL_H_
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <cublas_v2.h>
+#include <curand.h>
+
+namespace eigencuda {
+
+template <typename T>
+Mat<T> cublas_gemm(Mat<T> &A, Mat<T> &B, bool pinned = false) {
+  // Transfer the matrix matrix multiplacation of Eigen to GPU, using
+  // CUBLas
+
+  // Scalar constanst for calling blas
+  constexpr T alpha = 1.;
+  constexpr T beta = 0.;
+  const T *pa = &alpha;
+  const T *pb = &beta;
+
+  // Size of the Matrices
+  std::size_t size_A = A.rows() * A.cols() * sizeof(T);
+  std::size_t size_B = A.cols() * B.cols() * sizeof(T);
+  std::size_t size_C = A.rows() * B.cols() * sizeof(T);
+
+  Mat<T> C = Mat<T>::Zero(A.rows(), B.cols());
+
+  // and their pointers
+  T *hA = A.data();
+  T *hB = B.data();
+  T *hC = C.data();
+
+  // alloc memory on the GPU
+  T *dA, *dB, *dC;
+
+  // Allocate either pageable or pinned memory
+  auto fun_alloc = [&pinned](T **x, std::size_t n) {
+    (pinned) ? cudaMallocHost(x, n) : cudaMalloc(x, n);
+  };
+
+  fun_alloc(&dA, size_A);
+  fun_alloc(&dB, size_B);
+  fun_alloc(&dC, size_C);
+
+  // cuda handle
+  cublasHandle_t handle;
+  cublasCreate(&handle);
+
+  // Transfer data to GPU
+  cudaMemcpy(dA, hA, size_A, cudaMemcpyHostToDevice);
+  cudaMemcpy(dB, hB, size_B, cudaMemcpyHostToDevice);
+
+  // process on GPU
+  if constexpr (std::is_same<float, T>()) {
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A.rows(), B.cols(), A.cols(),
+                pa, dA, A.rows(), dB, B.rows(), pb, dC, C.rows());
+  } else if (std::is_same<double, T>()) {
+    cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A.rows(), B.cols(), A.cols(),
+                pa, dA, A.rows(), dB, B.rows(), pb, dC, C.rows());
+  }
+
+  // send data back to CPU
+  cudaMemcpy(hC, dC, size_C, cudaMemcpyDeviceToHost);
+
+  // create an eigen matrix
+  C = Eigen::Map<Mat<T>>(hC, A.rows(), B.cols());
+
+  // free memory
+  cublasDestroy(handle);
+
+  auto fun_free = [&pinned](T *x) { (pinned) ? cudaFreeHost(x) : cudaFree(x); };
+
+  fun_free(dA);
+  fun_free(dB);
+  fun_free(dC);
+
+  return C;
+}
+
+template <typename T>
+Mat<T> triple_product(Mat<T> &A, Mat<T> &B, Mat<T> &C, bool pinned = false) {
+  // Perform the triple matrix Multiplication: A^T * B * C
+
+  // Transfer the matrix matrix multiplacation of Eigen to GPU, using
+  // CUBLas
+
+  // Scalar constanst for calling blas
+  constexpr T alpha = 1.;
+  constexpr T beta = 0.;
+  const T *pa = &alpha;
+  const T *pb = &beta;
+
+  // Size of the Matrices
+  std::size_t size_A = A.rows() * A.cols() * sizeof(T);
+  std::size_t size_B = B.rows() * B.cols() * sizeof(T);
+  std::size_t size_C = C.rows() * C.cols() * sizeof(T);
+  std::size_t size_X = B.rows() * C.cols() * sizeof(T);
+  std::size_t size_Y = A.cols() * C.cols() * sizeof(T);
+
+  Mat<T> X = Mat<T>::Zero(B.rows(), C.cols());
+  Mat<T> Y = Mat<T>::Zero(A.cols(), C.cols());
+
+  // and their pointers
+  T *hA = A.data();
+  T *hB = B.data();
+  T *hC = C.data();
+  T *hY = C.data();
+
+  // alloc memory on the GPU
+  T *dA, *dB, *dC, *dX, *dY;
+
+  // Allocate either pageable or pinned memory
+  auto fun_alloc = [&pinned](T **x, std::size_t n) {
+    (pinned) ? cudaMallocHost(x, n) : cudaMalloc(x, n);
+  };
+
+  fun_alloc(&dA, size_A);
+  fun_alloc(&dB, size_B);
+  fun_alloc(&dC, size_C);
+  fun_alloc(&dX, size_X);
+  fun_alloc(&dY, size_Y);
+
+  // cuda handle
+  cublasHandle_t handle;
+  cublasCreate(&handle);
+
+  // Transfer data to GPU
+  cudaMemcpy(dA, hA, size_A, cudaMemcpyHostToDevice);
+  cudaMemcpy(dB, hB, size_B, cudaMemcpyHostToDevice);
+  cudaMemcpy(dC, hC, size_C, cudaMemcpyHostToDevice);
+
+  // multiplied in the GPU
+  if constexpr (std::is_same<float, T>()) {
+    // X = B * C
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, B.rows(), C.cols(), B.cols(),
+                pa, dB, B.rows(), dC, C.rows(), pb, dX, X.rows());
+    // R = A^T * X
+    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, A.cols(), X.cols(), A.rows(),
+                pa, dA, A.rows(), dX, X.rows(), pb, dY, Y.rows());
+  } else if (std::is_same<double, T>()) {
+    // X = B * C
+    cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, B.rows(), C.cols(), B.cols(),
+                pa, dB, B.rows(), dC, C.rows(), pb, dX, X.rows());
+    // R = A^T * X
+    cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, A.cols(), X.cols(), A.rows(),
+                pa, dA, A.rows(), dX, X.rows(), pb, dY, Y.rows());
+  }
+  // send data back to CPU
+  cudaMemcpy(hY, dY, size_Y, cudaMemcpyDeviceToHost);
+
+  // create an eigen matrix
+  Y = Eigen::Map<Mat<T>>(hY, A.cols(), C.cols());
+
+  // free memory
+  cublasDestroy(handle);
+
+  auto fun_free = [&pinned](T *x) { (pinned) ? cudaFreeHost(x) : cudaFree(x); };
+
+  fun_free(dA);
+  fun_free(dB);
+  fun_free(dC);
+  fun_free(dX);
+  fun_free(dY);
+
+  return Y;
+}
+
+}
+#endif // EIGENMATMUL_H_
