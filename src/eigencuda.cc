37e8b90bd79f3bc9889eacbc8a7697689e30baf8
felipez <tifonzafel@gmail.com>
2019-08-08 13:09:09 +0200

    added documentation

--- src/eigencuda.cc
+++ src/eigencuda.cc
@@ -2,31 +2,48 @@
 
 namespace eigencuda {
 
+/*
+ * Removed all the allocated arrays from the device
+ */  
 template <typename T> EigenCuda<T>::~EigenCuda() {
   cublasDestroy(_handle);
   for (auto &p : _allocated)
     this->gpu_free(p.second);
 }
 
+/*
+ * Allocate memory in the device using either pinned or pageable (default)
+ * memory
+ */   
 template <typename T> void EigenCuda<T>::gpu_alloc(T **x, std::size_t n) const {
-  // Allocate memory in the device
   (_pinned) ? cudaMallocHost(x, n) : cudaMalloc(x, n);
 }
 
+/*
+ * Deallocate memory from the device
+ */
 template <typename T> void EigenCuda<T>::gpu_free(T *x) const {
-  // Deallocate memory from the device
   (_pinned) ? cudaFreeHost(x) : cudaFree(x);
 };
 
+/*
+ * Release the memory associated with the pointer `id` and removed the pointer
+ * from the tracked pointers collection
+ */  
 template <typename T> void EigenCuda<T>::free_matrix(int id) {
   // Free Array with id from the device
   gpu_free(_allocated.at(id));
   _allocated.erase(id);
 }
 
+/*
+ * Allocate memory in the device for matrix A, then if if `copy_to_device`
+ * copy the array to the device. Sometimes it only neccesary to allocate
+ * space in the device without copying the array because the initial
+ * values may not be important like a temporal matrix.
+ */
 template <typename T>
 int EigenCuda<T>::initialize_Matrix(Mat<T> &A, bool copy_to_device) {
-  // Copy two matrices to the device
 
   // size of the Matrices
   std::size_t size_A = A.size() * sizeof(T);
@@ -51,7 +68,11 @@ int EigenCuda<T>::initialize_Matrix(Mat<T> &A, bool copy_to_device) {
 
   return id;
 }
-
+/*
+ * Call the gemm function from cublas, resulting in the multiplication of the 
+ * two matrices with identifiers id_A and id_B. The result is stored in
+ * a Matrix (pointer) with identifier id_C.
+ */
 template <typename T>
 void EigenCuda<T>::gemm(Shapes sh,
                         std::tuple<int, int, int> ids) {
@@ -83,9 +104,15 @@ void EigenCuda<T>::gemm(Shapes sh,
   }
 }
 
-template <typename T> Mat<T> EigenCuda<T>::dot(Mat<T> &A, Mat<T> &B) {
-  // Matrix multiplication
-
+/*
+ * Perform the matrix-matrix multiplication between A and B. First, 
+ * memory is allocated in the device for both matrices then a third temporal array
+ * is allocated in the device that will contain the results. Finally, the
+ * memory contains in the temporal result is copy back to the main memory and
+ * Free the resources
+ */
+template <typename T>
+Mat<T> EigenCuda<T>::dot(Mat<T> &A, Mat<T> &B) {
   // Matrix to store the result
   Mat<T> C = Mat<T>::Zero(A.rows(), B.cols());
   std::size_t size_C = C.size() * sizeof(T);
