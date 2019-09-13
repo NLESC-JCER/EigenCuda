eeafa88112b63a31e81775bc8bc8a0f512778060
felipe <tifonzafel@gmail.com>
2019-09-13 14:58:04 +0200

    clang-format

--- src/eigencuda.cc
+++ src/eigencuda.cc
@@ -1,5 +1,4 @@
 #include "eigencuda.hpp"
-#include <sstream>
 
 namespace eigencuda {
 
@@ -38,24 +37,25 @@ void EigenCuda<T>::check_available_memory(size_t requested) const {
 
   // Use Unified memory
   cudaMallocManaged(&free, sizeof(size_t));
-  cudaMallocManaged(&total, sizeof(size_t));  
+  cudaMallocManaged(&total, sizeof(size_t));
   checkCuda(cudaMemGetInfo(free, total));
 
   std::ostringstream oss;
-  oss << "There were requested : " << requested << "bytes int the device\n";  
-  oss << "Device Free memory (bytes): " << *free << "\nDevice total Memory (bytes): "  << *total << "\n";
+  oss << "There were requested : " << requested << "bytes int the device\n";
+  oss << "Device Free memory (bytes): " << *free
+      << "\nDevice total Memory (bytes): " << *total << "\n";
 
   // Raise an error if there is not enough total or free memory in the device
-  if (requested > *free)  {
+  if (requested > *free) {
     oss << "There is not enough memory in the Device!\n";
     throw std::runtime_error(oss.str());
   }
-  
+
   // Release memory
   cudaFree(free);
   cudaFree(total);
 }
-  
+
 /*
  * Allocate memory in the device for matrix A.
  */
@@ -81,17 +81,16 @@ T *EigenCuda<T>::initialize_and_copy(const Mat<T> &A) const {
   size_t size_A = A.size() * sizeof(T);
   T *dA = initialize_matrix_mem(size_A);
 
-  // Transfer data to the GPU 
-  const T *hA = A.data();   // Pointers at the host
+  // Transfer data to the GPU
+  const T *hA = A.data();  // Pointers at the host
   cudaError_t err =
-    cudaMemcpyAsync(dA, hA, size_A, cudaMemcpyHostToDevice, _stream);
+      cudaMemcpyAsync(dA, hA, size_A, cudaMemcpyHostToDevice, _stream);
   if (err != 0) {
     throw std::runtime_error("Error copy arrays to device");
   }
   return dA;
 }
-  
-  
+
 /*
  * Call the gemm function from cublas, resulting in the multiplication of the
  * two matrices.
@@ -104,7 +103,7 @@ void EigenCuda<T>::gemm(Shapes sh, const T *dA, const T *dB, T *dC) const {
   T _beta = 0.;
   const T *_palpha = &_alpha;
   const T *_pbeta = &_beta;
-  
+
   // call gemm from cublas
   if constexpr (std::is_same<float, T>()) {
     cublasSgemm(_handle, CUBLAS_OP_N, CUBLAS_OP_N, sh.A_rows, sh.B_cols,
@@ -168,17 +167,17 @@ std::vector<Mat<T>> EigenCuda<T>::right_matrix_tensor(
   // sizes of the matrices to allocated in the device
   size_t size_A = matrix.size() * sizeof(T);
   size_t size_B = B.size() * sizeof(T);
-  size_t size_C = matrix.rows() * B.cols() * sizeof(T);  
+  size_t size_C = matrix.rows() * B.cols() * sizeof(T);
 
   // Check if there is enough available memory
   check_available_memory(size_A + size_B + size_C);
-  
+
   // Initialize memory for tensor components
   T *dA = initialize_matrix_mem(size_A);
 
   // Allocate memory for the final result array C
   T *dC = initialize_matrix_mem(size_C);
-  
+
   // Copy Matrix B to the device
   T *dB = initialize_and_copy(B);
 
@@ -187,18 +186,20 @@ std::vector<Mat<T>> EigenCuda<T>::right_matrix_tensor(
 
   // Vector containing the results
   std::vector<Mat<T>> rs(batchCount, Mat<T>::Zero(matrix.rows(), B.cols()));
-  
+
   // Call tensor matrix multiplication
-  for (auto i=0; i < batchCount; i++) {
+  for (auto i = 0; i < batchCount; i++) {
     // Copy tensor component to the device
-    checkCuda(cudaMemcpyAsync(dA, tensor[i].data(), size_C, cudaMemcpyHostToDevice, _stream));
-    
+    checkCuda(cudaMemcpyAsync(dA, tensor[i].data(), size_C,
+                              cudaMemcpyHostToDevice, _stream));
+
     // matrix multiplication
     gemm(sh, dA, dB, dC);
 
     // Copy the result to the host
     T *hout = rs[i].data();
-    checkCuda(cudaMemcpyAsync(hout, dC, size_C, cudaMemcpyDeviceToHost, _stream));
+    checkCuda(
+        cudaMemcpyAsync(hout, dC, size_C, cudaMemcpyDeviceToHost, _stream));
   }
 
   // Deallocate all the memory from the device
@@ -221,38 +222,39 @@ std::vector<Mat<T>> EigenCuda<T>::triple_tensor_product(
 
   // First submatrix from the tensor
   Mat<T> matrix = tensor[0];
-  
+
   // sizes of the matrices to allocated in the device
   size_t size_A = A.size() * sizeof(T);
   size_t size_B = matrix.size() * sizeof(T);
-  size_t size_C = C.size() * sizeof(T);  
+  size_t size_C = C.size() * sizeof(T);
   std::size_t size_X = A.rows() * matrix.cols() * sizeof(T);
   std::size_t size_Y = A.rows() * C.cols() * sizeof(T);
 
   // Check if there is enough available memory
-  check_available_memory(size_A + size_B + size_C + size_X + size_Y);  
-  
+  check_available_memory(size_A + size_B + size_C + size_X + size_Y);
+
   // Copy Matrix B to the device
   T *dA = initialize_and_copy(A);
   T *dC = initialize_and_copy(C);
   T *dB = initialize_matrix_mem(size_B);
 
   // Intermediate result X
-  T *dX = initialize_matrix_mem(size_X) ;
-  
+  T *dX = initialize_matrix_mem(size_X);
+
   // Final result array Y
   T *dY = initialize_matrix_mem(size_Y);
 
   // Shapes of the matrices
   Shapes sh1{A.rows(), A.cols(), matrix.rows(), matrix.cols(), A.rows()};
-  Shapes sh2{A.rows(), matrix.cols(), C.rows(), C.cols(), A.rows()};  
+  Shapes sh2{A.rows(), matrix.cols(), C.rows(), C.cols(), A.rows()};
 
   // Vector containing the results
   std::vector<Mat<T>> rs(batchCount, Mat<T>::Zero(A.rows(), C.cols()));
 
-  for (auto i=0; i < batchCount; i++) {
+  for (auto i = 0; i < batchCount; i++) {
     // tensor component
-    checkCuda(cudaMemcpyAsync(dB, tensor[i].data(), size_B, cudaMemcpyHostToDevice, _stream));
+    checkCuda(cudaMemcpyAsync(dB, tensor[i].data(), size_B,
+                              cudaMemcpyHostToDevice, _stream));
 
     // Call the first tensor matrix multiplication
     gemm(sh1, dA, dB, dX);
@@ -262,7 +264,8 @@ std::vector<Mat<T>> EigenCuda<T>::triple_tensor_product(
 
     // Copy the result Array back to the device
     T *hout = rs[i].data();
-    checkCuda(cudaMemcpyAsync(hout, dY, size_Y, cudaMemcpyDeviceToHost, _stream));
+    checkCuda(
+        cudaMemcpyAsync(hout, dY, size_Y, cudaMemcpyDeviceToHost, _stream));
   }
 
   // Deallocate all the memory from the device
