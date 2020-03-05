1bfd1eecb279af7a604a68472e5783d612cf7c10
felipez <tifonzafel@gmail.com>
2020-03-05 17:03:08 +0100

    moved the common funcionality matrix/tensor to memory_man #24

--- include/cudamatrix.hpp
+++ include/cudamatrix.hpp
@@ -37,18 +37,11 @@ class CudaMatrix {
   void copy_to_gpu(const Eigen::MatrixXd &A);
 
  private:
-  // Unique pointer with custom delete function
-  using Unique_ptr_to_GPU_data = std::unique_ptr<double, void (*)(double *)>;
-
-  Unique_ptr_to_GPU_data alloc_matrix_in_gpu(size_t size_arr) const;
-
-  void throw_if_not_enough_memory_in_gpu(size_t requested_memory) const;
-
   size_t size_matrix() const { return this->size() * sizeof(double); }
 
   // Attributes of the matrix in the device
-  Unique_ptr_to_GPU_data _data{nullptr,
-                               [](double *x) { eigencuda::checkCuda(cudaFree(x)); }};
+  Unique_ptr_to_GPU_data _data{
+      nullptr, [](double *x) { eigencuda::checkCuda(cudaFree(x)); }};
   cudaStream_t _stream = nullptr;
   Index _rows;
   Index _cols;
--- src/cudamatrix.cc
+++ src/cudamatrix.cc
@@ -6,8 +6,7 @@ CudaMatrix::CudaMatrix(const Eigen::MatrixXd &matrix,
                        const cudaStream_t &stream)
     : _rows{static_cast<Index>(matrix.rows())},
       _cols{static_cast<Index>(matrix.cols())} {
-  _data = eigencuda::alloc_tensor_in_gpu(
-      size_matrix());  // alloc_matrix_in_gpu(size_matrix());
+  _data = eigencuda::alloc_tensor_in_gpu(size_matrix());
   _stream = stream;
   cudaError_t err = cudaMemcpyAsync(_data.get(), matrix.data(), size_matrix(),
                                     cudaMemcpyHostToDevice, stream);
@@ -18,14 +17,15 @@ CudaMatrix::CudaMatrix(const Eigen::MatrixXd &matrix,
 
 CudaMatrix::CudaMatrix(Index nrows, Index ncols, const cudaStream_t &stream)
     : _rows{static_cast<Index>(nrows)}, _cols{static_cast<Index>(ncols)} {
-  _data = alloc_matrix_in_gpu(size_matrix());
+  _data = eigencuda::alloc_tensor_in_gpu(size_matrix());
   _stream = stream;
 }
 
 CudaMatrix::operator Eigen::MatrixXd() const {
   Eigen::MatrixXd result = Eigen::MatrixXd::Zero(this->rows(), this->cols());
-  eigencuda::checkCuda(cudaMemcpyAsync(result.data(), this->data(), this->size_matrix(),
-                            cudaMemcpyDeviceToHost, this->_stream));
+  eigencuda::checkCuda(cudaMemcpyAsync(result.data(), this->data(),
+                                       this->size_matrix(),
+                                       cudaMemcpyDeviceToHost, this->_stream));
   eigencuda::checkCuda(cudaStreamSynchronize(this->_stream));
   return result;
 }
@@ -33,35 +33,7 @@ CudaMatrix::operator Eigen::MatrixXd() const {
 void CudaMatrix::copy_to_gpu(const Eigen::MatrixXd &A) {
   size_t size_A = static_cast<Index>(A.size()) * sizeof(double);
   eigencuda::checkCuda(cudaMemcpyAsync(this->data(), A.data(), size_A,
-                            cudaMemcpyHostToDevice, _stream));
-}
-
-CudaMatrix::Unique_ptr_to_GPU_data CudaMatrix::alloc_matrix_in_gpu(
-    size_t size_arr) const {
-  double *dmatrix;
-  throw_if_not_enough_memory_in_gpu(size_arr);
-  eigencuda::checkCuda(cudaMalloc(&dmatrix, size_arr));
-  Unique_ptr_to_GPU_data dev_ptr(dmatrix,
-                                 [](double *x) { eigencuda::checkCuda(cudaFree(x)); });
-  return dev_ptr;
-}
-
-void CudaMatrix::throw_if_not_enough_memory_in_gpu(
-    size_t requested_memory) const {
-  size_t free, total;
-  eigencuda::checkCuda(cudaMemGetInfo(&free, &total));
-
-  std::ostringstream oss;
-  oss << "There were requested : " << requested_memory
-      << "bytes Index the device\n";
-  oss << "Device Free memory (bytes): " << free
-      << "\nDevice total Memory (bytes): " << total << "\n";
-
-  // Raise an error if there is not enough total or free memory in the device
-  if (requested_memory > free) {
-    oss << "There is not enough memory in the Device!\n";
-    throw std::runtime_error(oss.str());
-  }
+                                       cudaMemcpyHostToDevice, _stream));
 }
 
 }  // namespace eigencuda
